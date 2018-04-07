7bd3013beddfa7c7ed833f2d1f3c4cde99b835fe
Carlos Andrés <carlos.andres.aguirre.c@gmail.com>
2018-04-07 13:06:15 -0500

    Se modifica los fondos del estado del menu y del estado principal, ademas de ingresar gravedad en el jugador principal.

--- js/estadoMenu.js
+++ js/estadoMenu.js
@@ -1,3 +1,4 @@
+var fondoMenu;
 var estadoMenu = {
 
       /**
@@ -7,17 +8,24 @@ var estadoMenu = {
         game.stage.backgroundcolor='#FFF';
         // Se carga el boton de start
         game.load.image('boton', 'img/btnStart.png');
-      /*  // Se carga el personaje principal
-        game.load.spritesheet('dude', 'img/dude2.png', 70,70);
-    */},
+        // Se carga el fondo del estado Menu
+        game.load.image('fondo11', 'img/sky.png');
+
+    },
 
     /**
      * Función encargada de mostrar los recursos en pantalla.
      */
     create:function() {
+        
+        fondoMenu = game.add.tileSprite(0,40,900,2000,'fondo11');
+
         var boton = this.add.button(game.width/2-160,game.height/2-94,'boton',this.iniciarJuego,this);
+        
+        
         var txtInicio = game.add.text(game.width/2,game.height/2-100,"GAME ADD",{font:"bold 34px Verdana",fill:"#c1c1c1",align:"center"});
         txtInicio.anchor.setTo(0.5);
+
     },
 
     iniciarJuego:function(){
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 var game = new Phaser.Game(736, 460, Phaser.AUTO, 'phaser-block');
 var fondo;
 var personaje;
+var salto;
 var estadoPrincipal = {
 
     /**
@@ -10,7 +11,7 @@ var estadoPrincipal = {
     preload:function() {
         game.stage.backgroundcolor="#000";
         // Se carga el fondo1
-        game.load.image('fondo1', 'img/fondo1.jpg');
+        game.load.image('fondo1', 'img/fondo3.jpg');
         // Se carga el personaje principal
         game.load.spritesheet('dude', 'img/dude2.png', 65,75);
     },
@@ -22,9 +23,17 @@ var estadoPrincipal = {
         // Se muestra el fondo1
         fondo = game.add.tileSprite(0,0,736,460,'fondo1');
         // Se muestra el personaje principal
-        personaje = game.add.sprite(200,350,'dude');
+        personaje = game.add.sprite(140,360,'dude');
         personaje.frame= 0;
         personaje.animations.add('correr',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],28,true);
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        game.physics.arcade.enable(personaje);
+       // game.physics.arcade.gravity.y = 0;
+        
+        salto = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        salto.onDown.add(this.saltar,this);
 
     },
 
@@ -39,9 +48,15 @@ var estadoPrincipal = {
     
     },
 
-    render:function() {
-
-
+    saltar:function() {
+        
+        personaje.body.velocity.y=-250;
+        game.time.now + 75000;
+        personaje.body.velocity.y=250;
+   //     personaje.body.velocity.x=150;
+//        personaje.body.velocity.y=350;
+  //      personaje.body.gravity.y = 600;
+       // personaje.body.gravity.y = 0;
     }
 
 }

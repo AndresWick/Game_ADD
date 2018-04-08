c7f6fa0a92d2791436c1052adc5afc5bd6494e96
AndresWick <carlos.andres.aguirre.c@gmail.com>
2018-04-08 10:47:57 -0500

    Se agrega la primer moneda del juego con gravedad y colision con el piso.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // https://phaser.io/news/2018/03/wedding-run
+//https://www.youtube.com/watch?v=nrDgPF2j_eo&index=19&list=PLdGlusD_3WpF9TUyLRcMOnU9jdZIi3y9a
 var game = new Phaser.Game(736, 460, Phaser.AUTO, 'phaser-block');
 var fondo;
 var personaje;
 var salto;
 var cursores;
 var plataformas;
+var monedas;
 var estadoPrincipal = {
 
     /**
@@ -16,9 +18,10 @@ var estadoPrincipal = {
         game.load.image('fondo1', 'img/fondo3.jpg');
         // Se carga el personaje principal
         game.load.spritesheet('dude', 'img/dude.png', 66,76);
-        
+        game.load.spritesheet('moneda', 'img/coin.png',30,30);
         game.load.image('tierra', 'img/tierra.png');
         game.load.image('plataforma1', 'img/plataforma1.png');
+
 
     },
 
@@ -30,74 +33,53 @@ var estadoPrincipal = {
         fondo = game.add.tileSprite(0,0,736,460,'fondo1');
         // Se muestra el personaje principal
         personaje = game.add.sprite(140,360-9,'dude');
-        
-      //  game.addTilesetImage('tierra');
-        
-        personaje.frame= 0;
+                
         personaje.animations.add('correrDerecha',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],28,true); 
         
         personaje.animations.add('correrIzquierda',[29,28,27,26,25,24,35,34,33,32,31,30,41,40,39,38,37,36,47,46,45,44,43,42],28,true);
         personaje.animations.add('detener',[48],28,true);
 
-        
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.physics.arcade.enable(personaje);
-        
+       
         plataformas = game.add.group();
         plataformas.enableBody = true;
+        // Se crea el piso del juego.
         var piso = plataformas.create(0,game.world.height-32,'tierra');
         piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(32,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(64,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(96,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(128,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(160,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(192,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(224,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(256,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(288,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(320,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(352,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(384,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(416,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(448,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(480,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(512,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(544,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(576,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(608,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(640,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(672,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        piso = plataformas.create(704,game.world.height-32,'tierra');
-        piso.scale.setTo(0.5,0.5);
-        
-        piso.body.inmovable = true;
+        for (var i=1;i<23;i++){
+            piso = plataformas.create(32*i,game.world.height-32,'tierra');
+            piso.scale.setTo(0.5,0.5);
+            piso.body.immovable = true;
+
+        }
         
         var barra = plataformas.create(350,200,'plataforma1');
-        barra.body.inmovable = false;
+        barra.body.immovable = false;
 
+        monedas = game.add.sprite(50,0,'moneda');
+        monedas.animations.add('spin', [0, 1, 2, 3, 4, 5, 6, 7], 28, true);
+    
+        
 
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.enable(personaje);
+        game.physics.arcade.enable(monedas);
+
+        
+        monedas.enableBody=true;
+        monedas.body.gravity.y=100;
+        monedas.body.bounce.setTo(0.8);
+        
+        
+	   // monedas.animations.play('spin');
+	   // 	monedas.body.immovable = true;
+	   // 	monedas.body.allowGravity = false;
+	   // 	monedas.body.checkCollision = false;
+	   // 	monedas.body.velocity.x = -this.game.Settings.physics.platformSpeed;
+ 
+		    //When the coin leaves the screen, kill it
+		 //   monedas.checkWorldBounds = true;
+		  //  monedas.outOfBoundsKill = true;
+        
         
         cursores = game.input.keyboard.createCursorKeys();
         salto = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -110,8 +92,15 @@ var estadoPrincipal = {
      * Función encargada de animar el juego.
      */
     update:function() {
+        
+        game.physics.arcade.collide(monedas,plataformas);
+        
+        
+        
+        
         // Se crea animación de movimiento en el fondo
-        fondo.tilePosition.x -= 1;
+       // fondo.tilePosition.x -= 1;
+        monedas.animations.play('spin');
         if(cursores.right.isDown){
            personaje.position.x+=2;
             personaje.animations.play('correrDerecha');
@@ -119,10 +108,7 @@ var estadoPrincipal = {
         else if(cursores.left.isDown){
            personaje.position.x-=2;
             personaje.animations.play('correrIzquierda');
-            //personaje.rotation+=5;
-
         }else{
-               personaje.position.x-=2;
             personaje.animations.play('detener');
         }
     

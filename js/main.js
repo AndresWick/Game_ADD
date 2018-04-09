2985802a1d197860880ae83023f996fdf437fef9
AndresWick <carlos.andres.aguirre.c@gmail.com>
2018-04-08 20:44:16 -0500

    Se agrega una segunda moneda de prueba.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var fondo;
 var personaje;
 var salto;
 var cursores;
+var pisos;
 var plataformas;
 var monedas;
 var estadoPrincipal = {
@@ -38,36 +39,51 @@ var estadoPrincipal = {
         
         personaje.animations.add('correrIzquierda',[29,28,27,26,25,24,35,34,33,32,31,30,41,40,39,38,37,36,47,46,45,44,43,42],28,true);
         personaje.animations.add('detener',[48],28,true);
-
        
-        plataformas = game.add.group();
-        plataformas.enableBody = true;
+        pisos = game.add.group();
+        pisos.enableBody = true;
         // Se crea el piso del juego.
-        var piso = plataformas.create(0,game.world.height-32,'tierra');
+        var piso = pisos.create(0,game.world.height-32,'tierra');
         piso.scale.setTo(0.5,0.5);
+        piso.body.immovable = true;
         for (var i=1;i<23;i++){
-            piso = plataformas.create(32*i,game.world.height-32,'tierra');
+            piso = pisos.create(32*i,game.world.height-32,'tierra');
             piso.scale.setTo(0.5,0.5);
             piso.body.immovable = true;
-
         }
         
+        plataformas = game.add.group();
+        plataformas.enableBody = true;
         var barra = plataformas.create(350,200,'plataforma1');
-        barra.body.immovable = false;
+        barra.body.immovable = true;
 
-        monedas = game.add.sprite(50,0,'moneda');
+        monedas = game.add.group();
+        monedas.enableBody = true;
+        monedas1 = game.add.sprite(50,0,'moneda');
+        monedas = game.add.sprite(350,0,'moneda');
         monedas.animations.add('spin', [0, 1, 2, 3, 4, 5, 6, 7], 28, true);
-    
+        monedas1.animations.add('spin', [0, 1, 2, 3, 4, 5, 6, 7], 28, true);
+
         
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.enable(personaje);
+        game.physics.arcade.enable(plataformas);
         game.physics.arcade.enable(monedas);
+        game.physics.arcade.enable(monedas1);
+
 
         
         monedas.enableBody=true;
         monedas.body.gravity.y=100;
         monedas.body.bounce.setTo(0.8);
+                
+        monedas1.enableBody=true;
+        monedas1.body.gravity.y=100;
+        monedas1.body.bounce.setTo(0.8);
+        
+        personaje.body.gravity.y=1000;
+        personaje.body.bounce.setTo(0);
         
         
 	   // monedas.animations.play('spin');
@@ -93,14 +109,21 @@ var estadoPrincipal = {
      */
     update:function() {
         
+        game.physics.arcade.collide(monedas,pisos);
+        game.physics.arcade.collide(monedas1,pisos);
+        game.physics.arcade.collide(monedas1,personaje);
+        game.physics.arcade.collide(personaje,pisos);
         game.physics.arcade.collide(monedas,plataformas);
-        
+        game.physics.arcade.collide(personaje,plataformas);
+
         
         
         
         // Se crea animación de movimiento en el fondo
        // fondo.tilePosition.x -= 1;
         monedas.animations.play('spin');
+        monedas1.animations.play('spin');
+
         if(cursores.right.isDown){
            personaje.position.x+=2;
             personaje.animations.play('correrDerecha');
@@ -118,8 +141,7 @@ var estadoPrincipal = {
     */
     saltar:function() {
         game.physics.arcade.collide(personaje,personaje);
-        personaje.body.velocity.y=-250;
-        salto = game.time.now + 750;
+        personaje.body.velocity.y=-450;
     }
 
 }
